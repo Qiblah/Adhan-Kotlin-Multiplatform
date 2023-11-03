d35e4be8bc0bb6afc3845e157683ed7064c71c26
Farouk <117727520+Farouk-abichou@users.noreply.github.com>
2023-11-04 00:22:19 +0100

    trying thing out

--- shared/src/commonMain/kotlin/org/adhan/application/Platform.kt
+++ shared/src/commonMain/kotlin/org/adhan/application/Platform.kt
@@ -1,10 +1,16 @@
 package org.adhan.application
 
+import kotlinx.datetime.TimeZone
+
 
 expect val platform: String
 
 class Greeting {
+    val timeZone : TimeZone.Companion = TimeZone
+
     fun greeting() = "Hello, $platform!"
+
+    fun myTimeZone() = timeZone.availableZoneIds
 
 }
 
